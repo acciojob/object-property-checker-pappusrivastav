dd5a512ca041a69991e5d110df98e066e18e6463
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-11-02 17:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+// const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
+
+// function hasKey(key) {
+//   //   write your code here
+// }
+
+// // Do not change the code below
+// const key = prompt("Enter Key.");
+// alert(hasKey(key));
+
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-  //   write your code here
+  return Object.prototype.hasOwnProperty.call(sampleObject, key);
 }
 
 // Do not change the code below
